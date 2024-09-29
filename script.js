296d4af539c99383295277760a729015e94db133
davisin-06 <168154808+davisin-06@users.noreply.github.com>
2024-09-29 17:05:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,415 @@
+
+let lista_lugares_objetos = [
+    {
+        nome: "Agulha de Cope",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Cabo cautério",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Cabo de bisturi",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Pinça curva",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Pinça reta",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Afastador farabeuf",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Tesoura angulada",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Dennis Brown",
+        lugar: "Armário 1 coluna 2 espaço 2"
+    },
+    {
+        nome: "Estilete",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Buldog",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Tentacânula",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Jansen",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Coli coração",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Coli oval",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Tesoura iris",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Dente de rato",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Dissecação",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Anatômicas",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Maguil",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Pedículo",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Mixter",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Longuete",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Durval",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Kocher",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Babycook",
+        lugar: "Armário 1 coluna 2 espaço 3"
+    },
+    {
+        nome: "Finochietto",
+        lugar: "Armário 1 coluna 2 espaço 4"
+    },
+    {
+        nome: "Gosset",
+        lugar: "Armário 1 coluna 2 espaço 4"
+    },
+    {
+        nome: "Alicate de pressão",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Alicate universal",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Cortador de fio",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Trefina",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Agulha de biópsia",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Turquesa",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Tesoura de gesso",
+        lugar: "Armário 1 coluna 2 espaço 5"
+    },
+    {
+        nome: "Afastador de garra",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Backaus",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Tesoura plástica",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Pinça cheron",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Porta agulha",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Tesoura metzembaum",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Pinça satinski",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Allis",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Pinça Adson",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Kelly curva",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Kelly reta",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Pinça foerster",
+        lugar: "Armário 1 coluna 1 espaço 3"
+    },
+    {
+        nome: "Passagem de fio",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Gancho de osso",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Curetas",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Descolador de osso",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Martelo",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Osteóstomo",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Kerrison",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Formão",
+        lugar: "Armário 1 coluna 1 espaço 4"
+    },
+    {
+        nome: "Cizalha",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Goiva",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Sacabocado",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Pegador de osso",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Brocas",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Afastador hohmann",
+        lugar: "Armário 1 espaço 1 coluna 5"
+    },
+    {
+        nome: "Algodão",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Gaze rabo",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Pasta de algodão",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Isopito",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Koban",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Máscara ctq",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Gaze queijo",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Micropore",
+        lugar: "Armário 1 coluna 3 espaço 1"
+    },
+    {
+        nome: "Chave L",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Chave boca",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Impactor",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Pegador de osso",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Extrator",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Régua ortopédica",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Mandril avulso",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Kai Kai",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Régua ortopédica",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Chave sextavada",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Chave da",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Afastador langenbeck",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Serra de gigli + gancho",
+        lugar: "Armário 1 coluna 3 espaço 2"
+    },
+    {
+        nome: "Ponta de bisturi",
+        lugar: "Armário 1 coluna 3 espaço 3"
+    },
+    {
+        nome: "Estimulador periférico",
+        lugar: "Armário 1 coluna 3 espaço 3"
+    },
+    {
+        nome: "Caneta de bisturi",
+        lugar: "Armário 1 coluna 3 espaço 3"
+    },
+    {
+        nome: "Bico de aspirador",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Debakey",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Autostáticos",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Pinça randall",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Alicate Lane",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Pinça de redução",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Afastador ortopédico",
+        lugar: "Armário 1 coluna 4 espaço 2"
+    },
+    {
+        nome: "Punho de foco",
+        lugar: "Armário 1 coluna 4 espaço 1"
+    },
+    {
+        nome: "Faixa de smarch",
+        lugar: "Armário 1 coluna 4 espaço 1"
+    },
+
+]
+
+
+function Procurar() {
+    var input = document.getElementById("input").value.trim();
+    const caixa = document.getElementById("conteiner");
+    var objetoEncontrado = null;
+    var lista_criada = document.getElementById("lista_objeto");
+    if (lista_criada) caixa.removeChild(lista_criada);
+    var lista_ul = document.createElement('ul')
+    lista_ul.id = "lista_objeto";
+    caixa.appendChild(lista_ul)
+    debugger 
+    for (let posicao = 0; posicao < lista_lugares_objetos.length; posicao = posicao + 1) {
+        if (input == lista_lugares_objetos[posicao].nome) {
+            objetoEncontrado = lista_lugares_objetos[posicao]
+            break
+        }
+        
+        if (input == "")break;
+    }
+    debugger
+    
+    if (objetoEncontrado != null) {
+        const Informacao = `<p><b>${objetoEncontrado.lugar}.</b></p>         `
+        lista_ul.innerHTML = Informacao;
+    }
+
+    
+    else {
+        const erro = `<p>"${input}" não é um instrumento ou não foi encontrado!</p>
+        `
+        lista_ul.innerHTML = erro;
+    }
+
+    if (input == "") {
+        lista_ul.innerHTML = "O instrumento não foi encontrado!"
+    }
+    
+}
